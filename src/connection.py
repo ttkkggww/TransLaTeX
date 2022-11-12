71ea8f531da0a6056af218c60fc777c76951b41a
ttkkggww <takigawaktai@gmail.com>
2022-11-12 19:03:12 +0900

    create connection

---
+++ src/connection.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import os
+
+class Connection:
+    def __init__(self):
+        api_json = json.load(open("api.json",'r'))
+        self.api_key = api_json["deepl_api"]
+        print(self.api_key)
+        print(type(self.api_key))
+
+    def translate(self,text:str,source_lang:str = "JA",target_lang:str = "EN"):
+        params = {
+                "auth_key" : self.api_key,
+                "text" : text,
+                "source_lang" : source_lang, 
+                "target_lang": target_lang 
+                }
+        request = requests.post("https://api-free.deepl.com/v2/translate", data=params)
+        result = request.json()
+        print(result)
+
+
+    
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from src.connection import Connection
+
+def main():
+    connection = Connection()
+    connection.translate("hello","EN","JA")
+
+if __name__ == "__main__":
+    main()
